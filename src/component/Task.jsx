6861af45d76d64574bcc65e333e3e83676f89060
Giovanni <giuliana.pinton@gmail.com>
2024-12-11 16:50:20 -0300

    💄style: fixing style in checkbox of the Task component (now completely working)

--- src/component/Task.jsx
+++ src/component/Task.jsx
@@ -1,10 +1,14 @@
 import styles from "./Task.module.css"
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from "@fortawesome/free-solid-svg-icons"
+
 function Task(props){
     return(
         <div className={styles.task}>
             <input type="checkbox"/>
             <p>{props.content}</p>
+            <FontAwesomeIcon icon={faTrash} className={styles.icon}/>
         </div>
     )
 }
